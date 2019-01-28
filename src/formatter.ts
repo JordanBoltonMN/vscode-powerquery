15cb314e3b35e01eaa269c90ef7cbcd9878d375a
JordanBoltonMN <jordanboltonmn@gmail.com>
2019-01-27 16:14:22 -0800

    getting there

--- src/formatter.ts
+++ src/formatter.ts
@@ -1,6 +1,6 @@
-// import { format, FormatRequest, IndentationLiteral, NewlineLiteral, Result, ResultType, SerializerOptions } from "pq-fmt-core";
+import { format, FormatRequest, IndentationLiteral, NewlineLiteral, Result, ResultType, SerializerOptions } from "pq-fmt-core";
 import * as vscode from "vscode";
-import { CancellationToken, DocumentFilter, DocumentFormattingEditProvider, DocumentSelector, FormattingOptions, Range, TextDocument, TextEdit } from "vscode";
+import { CancellationToken, DocumentFormattingEditProvider, FormattingOptions, Range, TextDocument, TextEdit } from "vscode";
 
 export class PowerQueryEditProvider implements DocumentFormattingEditProvider {
     provideDocumentFormattingEdits(
@@ -8,54 +8,42 @@ export class PowerQueryEditProvider implements DocumentFormattingEditProvider {
         options: FormattingOptions,
         _: CancellationToken
     ): Promise<TextEdit[]> {
-        return Promise.resolve([
-            TextEdit.replace(
-                fullDocumentRange(document),
-                "hi",
-            )
-        ]);
+        const documentText = document.getText();
+        if (!documentText) {
+            return Promise.resolve([]);
+        }
 
-        // let indentationLiteral;
-        // if (options.insertSpaces) {
-        //     indentationLiteral = IndentationLiteral.SpaceX4;
-        // }
-        // else {
-        //     indentationLiteral = IndentationLiteral.Tab;
-        // }
+        let indentationLiteral;
+        if (options.insertSpaces) {
+            indentationLiteral = IndentationLiteral.SpaceX4;
+        }
+        else {
+            indentationLiteral = IndentationLiteral.Tab;
+        }
 
-        // const serializerOptions: SerializerOptions = {
-        //     indentationLiteral,
-        //     newlineLiteral: NewlineLiteral.Unix,
-        // };
-        // const formatRequest: FormatRequest = {
-        //     document: document.getText(),
-        //     options: serializerOptions,
-        // }
-        // const formatResult: Result<string, string> = format(formatRequest);
-        // if (formatResult.type === ResultType.Ok) {
-        //     return Promise.resolve([
-        //         TextEdit.replace(
-        //             fullDocumentRange(document),
-        //             formatResult.value,
-        //         )
-        //     ]);
-        // }
-        // else {
-        //     vscode.window.showInformationMessage("An error has occured while formatting");
-        //     console.log(formatResult.error);
-        //     return Promise.reject("An error occured while formatting");
-        // }
+        const serializerOptions: SerializerOptions = {
+            indentationLiteral,
+            newlineLiteral: NewlineLiteral.Unix,
+        };
+        const formatRequest: FormatRequest = {
+            document: documentText,
+            options: serializerOptions,
+        }
+        const formatResult: Result<string, string> = format(formatRequest);
+        if (formatResult.type === ResultType.Ok) {
+            return Promise.resolve([
+                TextEdit.replace(
+                    fullDocumentRange(document),
+                    formatResult.value,
+                )
+            ]);
+        }
+        else {
+            vscode.window.showInformationMessage("An error has occured while formatting");
+            console.log(formatResult.error);
+            return Promise.reject("An error occured while formatting");
+        }
     }
-}
-
-export function powerQueryDocumentSelector(): DocumentSelector {
-    const languages = [
-        "pq",
-        "m",
-        "mout",
-    ];
-    const selectors: DocumentFilter[] = languages.map(l => ({ language: l }));
-    return selectors;
 }
 
 function fullDocumentRange(document: TextDocument): Range {
