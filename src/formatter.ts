a70a6cf3aa7372a1695d164e6417287b07de1054
JordanBoltonMN <jordanboltonmn@gmail.com>
2019-02-27 21:04:49 -0800

    works on a good chunk of test files, still not complete

--- src/formatter.ts
+++ src/formatter.ts
@@ -1,52 +1,67 @@
-import { format, FormatRequest, IndentationLiteral, NewlineLiteral, Result, ResultType, SerializerOptions } from "pq-fmt-core";
+import * as pqFmt from "pq-fmt-core";
 import * as vscode from "vscode";
-import { CancellationToken, DocumentFormattingEditProvider, FormattingOptions, Range, TextDocument, TextEdit } from "vscode";
+import {
+  CancellationToken,
+  DocumentFormattingEditProvider,
+  FormattingOptions,
+  Range,
+  TextDocument,
+  TextEdit
+} from "vscode";
 
 export class PowerQueryEditProvider implements DocumentFormattingEditProvider {
-    provideDocumentFormattingEdits(
-        document: TextDocument,
-        options: FormattingOptions,
-        _: CancellationToken
-    ): Promise<TextEdit[]> {
-        const documentText = document.getText();
-        if (!documentText) {
-            return Promise.resolve([]);
-        }
-
-        let indentationLiteral;
-        if (options.insertSpaces) {
-            indentationLiteral = IndentationLiteral.SpaceX4;
-        }
-        else {
-            indentationLiteral = IndentationLiteral.Tab;
-        }
-
-        const serializerOptions: SerializerOptions = {
-            indentationLiteral,
-            newlineLiteral: NewlineLiteral.Unix,
-        };
-        const formatRequest: FormatRequest = {
-            document: documentText,
-            options: serializerOptions,
-        }
-        const formatResult: Result<string, string> = format(formatRequest);
-        if (formatResult.type === ResultType.Ok) {
-            return Promise.resolve([
-                TextEdit.replace(
-                    fullDocumentRange(document),
-                    formatResult.value,
-                )
-            ]);
-        }
-        else {
-            vscode.window.showInformationMessage("An error has occured while formatting");
-            console.log(formatResult.error);
-            return Promise.reject("An error occured while formatting");
-        }
+  provideDocumentFormattingEdits(
+    document: TextDocument,
+    options: FormattingOptions,
+    _: CancellationToken
+  ): Promise<TextEdit[]> {
+    const documentText = document.getText();
+    if (!documentText) {
+      return Promise.resolve([]);
     }
+
+    let indentationLiteral;
+    if (options.insertSpaces) {
+      indentationLiteral = pqFmt.IndentationLiteral.SpaceX4;
+    } else {
+      indentationLiteral = pqFmt.IndentationLiteral.Tab;
+    }
+
+    const serializerOptions: pqFmt.SerializerOptions = {
+      indentationLiteral,
+      newlineLiteral: pqFmt.NewlineLiteral.Unix
+    };
+    const formatRequest: pqFmt.FormatRequest = {
+      document: documentText,
+      options: serializerOptions
+    };
+    const formatResult: pqFmt.Result<string, pqFmt.TFormatError> = pqFmt.format(
+      formatRequest
+    );
+    if (formatResult.kind === pqFmt.ResultKind.Ok) {
+      return Promise.resolve([
+        TextEdit.replace(fullDocumentRange(document), formatResult.value)
+      ]);
+    } else {
+      const error = formatResult.error;
+      console.error(error);
+
+      let informationMessage;
+      if (error instanceof pqFmt.LexerError) {
+        informationMessage = error.innerError.message;
+      } else if (error instanceof pqFmt.ParserError) {
+        informationMessage = error.innerError.message;
+      } else {
+        informationMessage = "An unknown error occured during formatting.";
+      }
+
+      vscode.window.showInformationMessage(informationMessage);
+      return Promise.reject(error);
+    }
+  }
 }
 
 function fullDocumentRange(document: TextDocument): Range {
-    const lastLineId = document.lineCount - 1;
-    return new Range(0, 0, lastLineId, document.lineAt(lastLineId).text.length);
+  const lastLineId = document.lineCount - 1;
+  return new Range(0, 0, lastLineId, document.lineAt(lastLineId).text.length);
 }
